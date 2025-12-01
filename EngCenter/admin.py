fbd3b6f04443f37ac7368f8d75336de6da4a5547
Website Admin <tranphuongvy03092005@gmail.com>
2025-12-02 00:44:25 +0700

    create

--- EngCenter/admin.py
+++ EngCenter/admin.py
@@ -13,15 +13,11 @@ from EngCenter.models.models import Course, User, Bill, Enrollment
 class MyAdminIndexView(AdminIndexView):
     pass
 
-class CourseView(ModelView):
-    extra_css = ['static/css/admin_course.css']
-    list_template = 'admin/model/course_list.html'
-
 admin = Admin(app=app, name="E-COMMERCE", theme=Bootstrap4Theme())
 
 category_QLDuLieu= 'Quản lý dữ liệu'
 
-admin.add_view(CourseView(Course, db.session,category=category_QLDuLieu))
+admin.add_view(ModelView(Course, db.session,category=category_QLDuLieu))
 admin.add_view(ModelView(User, db.session,category=category_QLDuLieu))
 
 category_ThongKe= 'Thống kê'
