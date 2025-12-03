4b9293b98cacbeba200d6c8bb9b2a2a15786fa13
Website Admin <tranphuongvy03092005@gmail.com>
2025-12-04 02:26:03 +0700

    create

--- EngCenter/admin.py
+++ EngCenter/admin.py
@@ -3,7 +3,6 @@ from flask_admin.contrib.sqla import ModelView
 from flask_admin import Admin, AdminIndexView, expose, BaseView
 from flask_admin.menu import MenuLink
 from flask_admin.theme import Bootstrap4Theme
-from unicodedata import category
 from flask_login import current_user, logout_user
 
 from EngCenter import db, app
@@ -15,16 +14,26 @@ class MyAdminIndexView(AdminIndexView):
 
 class CourseView(ModelView):
     list_template = 'admin/model/course_list.html'
+    create_template = 'admin/model/course_create.html'
 
-admin = Admin(app=app, name="E-COMMERCE", theme=Bootstrap4Theme())
+class UserView(ModelView):
+    list_template = 'admin/model/course_list.html'
+
+class BillView(ModelView):
+    list_template = 'admin/model/course_list.html'
+
+class EnrollmentView(ModelView):
+    list_template = 'admin/model/course_list.html'
+
+admin = Admin(app=app, theme=Bootstrap4Theme())
 
 category_QLDuLieu= 'Quản lý dữ liệu'
 
 admin.add_view(CourseView(Course, db.session,category=category_QLDuLieu))
-admin.add_view(ModelView(User, db.session,category=category_QLDuLieu))
+admin.add_view(UserView(User, db.session,category=category_QLDuLieu))
 
 category_ThongKe= 'Thống kê'
 
-admin.add_view(ModelView(Bill, db.session,category=category_ThongKe))
-admin.add_view(ModelView(Enrollment, db.session,category=category_ThongKe))
+admin.add_view(BillView(Bill, db.session,category=category_ThongKe))
+admin.add_view(EnrollmentView(Enrollment, db.session,category=category_ThongKe))
 
