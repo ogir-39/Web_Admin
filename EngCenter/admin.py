df0e9ab64b9f16f2e3f08d55ce7015da25283905
Website Admin <tranphuongvy03092005@gmail.com>
2025-12-04 20:27:53 +0700

    create

--- EngCenter/admin.py
+++ EngCenter/admin.py
@@ -12,31 +12,53 @@ from EngCenter.models.models import Course, User, Bill, Enrollment
 class MyAdminIndexView(AdminIndexView):
     pass
 
-class CourseView(ModelView):
+class SharedView(ModelView):
     list_template = 'admin/model/list.html'
     create_template = 'admin/model/create.html'
     edit_template = 'admin/model/edit.html'
     delete_template = 'admin/model/delete.html'
     extra_css = ['/static/admin/css/model_view.css']
 
-class UserView(ModelView):
-    list_template = 'admin/model/list.html'
+class CourseView(SharedView):
+    column_searchable_list = ["name"]
+    column_labels = {
+        'name': 'Tên khoá học',
+        'fee' : 'Học phí',
+        'age' : 'Độ tuổi',
+        'level' : 'Trình độ',
+        'duration hour' : 'Thời gian',
+        'course_description': 'Mô tả khoá học'
+    }
 
-class BillView(ModelView):
-    list_template = 'admin/model/list.html'
+class UserView(SharedView):
+    column_list = ['fullname','email','gender','phone_number','dob','address','status','type']
+    column_searchable_list = ['fullname','email','gender','phone_number','dob','address','status','type']
+    column_labels = {
+        'fullname' : 'Họ tên',
+        'gender' : 'Giới tính',
+        'phone_number' : 'Số điện thoại',
+        'dob' : 'Ngày sinh',
+        'address' : 'Địa chỉ',
+        'status' : 'Trạng thái',
+        'type' : 'Vai trò'
+    }
 
-class EnrollmentView(ModelView):
-    list_template = 'admin/model/list.html'
+class BillView(SharedView):
+    pass
+
+class EnrollmentView(SharedView):
+    pass
+
 
 admin = Admin(app=app, theme=Bootstrap4Theme())
 
 category_QLDuLieu= 'Quản lý dữ liệu'
 
-admin.add_view(CourseView(Course, db.session,category=category_QLDuLieu))
-admin.add_view(UserView(User, db.session,category=category_QLDuLieu))
+admin.add_view(CourseView(Course, db.session,category=category_QLDuLieu,name="Khoá học"))
+admin.add_view(UserView(User, db.session,category=category_QLDuLieu,name="Tài khoản"))
 
 category_ThongKe= 'Thống kê'
 
-admin.add_view(BillView(Bill, db.session,category=category_ThongKe))
-admin.add_view(EnrollmentView(Enrollment, db.session,category=category_ThongKe))
+admin.add_view(BillView(Bill, db.session,category=category_ThongKe,name="Hoá đơn"))
+admin.add_view(EnrollmentView(Enrollment, db.session,category=category_ThongKe,name="Đăng ký"))
 
